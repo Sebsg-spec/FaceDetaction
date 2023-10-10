5f234906c8599ad522e96bb5e08594828f840a42
sebsg-spec <popasebi122@gmail.com>
2023-10-10 15:50:09 +0300

    Changes

---
+++ src/Components/Logo/Logo.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Tilt from 'react-parallax-tilt';
+import brain from './Brain.png'
+import './Logo.css'
+
+const Logo = () =>{
+    return(
+        <div className="ma4 mt0">
+            <Tilt className="Tilt br shadow-2" options ={{max : 55}} style = {{height: 150, width: 150}} >
+                <div className="Tilt-inner pa3">
+                    <img style={{paddingTop: '5px'}} alt="brain icon" src={brain}/>
+                </div>
+            </Tilt>
+        </div>
+
+
+
+    );        
+    }
+    export default Logo;
